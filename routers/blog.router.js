f8e1970844620b341a93cdd3110bcd9a6df0bf30
Rama Astra <ramaastra333@gmail.com>
2023-07-27 14:47:54 +0800

    Refactoring Blog Pages Router

--- routers/blog.router.js
+++ routers/blog.router.js
@@ -3,8 +3,8 @@ const router = Router()
 const blogController = require('../controllers/blog.controller')
 
 router.get('/blog', blogController.listPage)
-router.get('/blog/read/:id', blogController.detailPage)
-router.get('/blog/create', (blogController.createPage))
-router.get('/blog/edit/:id', (blogController.editPage))
+router.get('/blog/create', blogController.createPage)
+router.get('/blog/:id/read', blogController.detailPage)
+router.get('/blog/:id/edit', blogController.editPage)
 
 module.exports = router

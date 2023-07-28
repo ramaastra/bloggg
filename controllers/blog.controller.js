c165bffbc48ec723747d54aee40dc1d69e5b7dde
Rama Astra <ramaastra333@gmail.com>
2023-07-28 17:16:55 +0800

    Adding Read and Update Functionality to Blog

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,30 +1,76 @@
-const dummyBlogs = require('./dummyBlogs')
-const dummyCategories = require('./dummyCategories')
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
 
 class BlogController {
   static listPage = async(req, res) => {
-    const blogs = dummyBlogs
+    const blogs = await prisma.blog.findMany({
+      include: {
+        category: true,
+        author: true
+      }
+    })
+
     res.render('pages/blog/list', { blogs })
   }
 
   static detailPage = async(req, res) => {
-    const blog = dummyBlogs
-      .find(blog => blog.id == req.params.id)
-    const otherBlogs = dummyBlogs
-      .filter(blog => blog.id != req.params.id)
-      .splice(0, 10)
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: Number(req.params.id)
+      },
+      include: {
+        category: true,
+        author: true
+      }
+    })
+    const otherBlogs  = await prisma.blog.findMany({
+      where: {
+        NOT: {
+          id: Number(req.params.id)
+        }
+      },
+      take: 10
+    })
 
     res.render('pages/blog/detail', { blog, otherBlogs })
   }
 
   static createPage = async(req, res) => {
-    res.render('pages/blog/create', { categories: dummyCategories })
+    const categories = await prisma.category.findMany()
+    res.render('pages/blog/create', { categories })
   }
 
   static editPage = async(req, res) => {
-    const blog = dummyBlogs
-      .find(blog => blog.id == req.params.id)
-    res.render('pages/blog/edit', { blog, categories: dummyCategories })
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: Number(req.params.id)
+      },
+      include: {
+        category: true
+      }
+    })
+    const categories = await prisma.category.findMany()
+
+    res.render('pages/blog/edit', { blog, categories })
+  }
+
+  static async update(req, res) {
+    const updatedBlog = {
+      title: req.body.title,
+      body: req.body.body.replace(/\r\n/g, '\n'),
+      categoryId: Number(req.body.category)
+    }
+    
+    if(req.file) updatedBlog.image = req.file.filename
+
+    await prisma.blog.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: updatedBlog
+    })
+
+    res.redirect(`/blog/${req.params.id}/read`)
   }
 }
 
--- routers/blog.router.js
+++ routers/blog.router.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const router = Router()
 const blogController = require('../controllers/blog.controller')
+const upload = require('../middlewares/upload')
 
 router.get('/blog', blogController.listPage)
 router.get('/blog/create', blogController.createPage)
 router.get('/blog/:id/read', blogController.detailPage)
 router.get('/blog/:id/edit', blogController.editPage)
+router.post('/blog/:id/edit', upload.single('img'), blogController.update)
 
 module.exports = router
